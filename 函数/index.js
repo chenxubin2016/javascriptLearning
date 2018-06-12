191a388c565fac9040c68cb28a93f64a383a6735
chenxubin <tybxzs@126.com>
2018-06-12 08:48:47 +0800

    20180612

---
+++ 函数/index.js
@@ -0,0 +1,41 @@
+/**函数*****************************************/
+
+/* 
+定义
+
+    函数声明 function test(){}
+
+    函数表达式 var test=function(){}
+
+组成形式
+
+    函数名称 不能以数字开头，最好用英文小驼峰命名
+
+    参数
+
+        形参
+
+        实参
+
+    返回值 
+*/
+
+// function test(){
+//     console.log('a');
+// }
+// test();
+
+// var test=function abc(){
+//     console.log('b');
+// }
+// abc()
+
+
+// 正确使用方法
+// var test=function () {
+//     console.log('c')
+// };
+// test()
+
+
+// index.js:40 Uncaught TypeError: test is not a function at
