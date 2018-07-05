1394513ec2604e4d10229b2daa3af9445b55e30e
chenxubin <tybxzs@126.com>
2018-07-05 16:47:35 +0800

    2018-07-05

--- tools.js
+++ tools.js
@@ -2,7 +2,7 @@
 *非空校验
 *isEmpty返回值为false说明字符串不为空，返回true字符串为空
 */
-function isEmpty(str) {
+function isEmpty (str) {
     if (str != null && str != undefined && str != "") {
         return false;
     } else if (str.replace(/(^s*)|(s*$)/g, "").length != 0) {
@@ -40,7 +40,7 @@ Array.prototype.max = function () {
  *
  * @returns
  */
-function sum() {
+function sum () {
     var result = 0;
     for (var i = 0; i < arguments.length; i++) {
         result += arguments[i];
@@ -53,7 +53,7 @@ function sum() {
  * @param {any} num 为任意值
  * @returns 布尔值
  */
-function isNaN(num) {
+function isNaN (num) {
     var ret = Number(num);
     ret += '';
     if (ret == 'NaN') {
@@ -68,7 +68,7 @@ function isNaN(num) {
  * @param {any} str 字符串
  * @returns 
  */
-function retBytes(str) {
+function retBytes (str) {
     var num = str.length;
     for (var i = 0; i < str.length; i++) {
         if (str.charCodeAt(i) > 255) {
@@ -83,7 +83,7 @@ function retBytes(str) {
  * @param {any} arr //指定数组
  * @returns 
  */
-function randomArr(arr) {
+function randomArr (arr) {
     var Arr = arr.soft(function (a, b) {
         return Math.round() - 0.5;
     })
@@ -105,13 +105,12 @@ Array.prototype.unique = function () {
     }
     return arr;
 }
-
 /**
  * 判断数据类型
  * @param {any} target 
  * @returns 
  */
-function type(target) {
+function type (target) {
     var ret = typeof (target);
     var template = {
         "[object Array]": "array",
@@ -136,7 +135,7 @@ function type(target) {
  * @param {*} nodeType 
  * @returns
  */
-function nodeTypeCodeToName(nodeType) {
+function nodeTypeCodeToName (nodeType) {
     switch (nodeType) {
         case 1:
             return '元素节点'
@@ -159,7 +158,7 @@ function nodeTypeCodeToName(nodeType) {
  * @param {*} n 第几个父级元素
  * @returns
  */
-function retParent(el, n) {
+function retParent (el, n) {
     while (el && n) {
         var el = el.parentElement;
         n--;
@@ -190,7 +189,7 @@ Element.prototype.myChildren = function () {
  * @param {*} el 对象
  * @param {*} n 整数
  */
-function retSibling(el, n) {
+function retSibling (el, n) {
     while (el && n) {
         if (n > 0) {
             if (el.nextElementSibling) {
@@ -261,7 +260,7 @@ Element.prototype.firstElementChild = function () {
  * pageOffset封装
  * 活动屏幕滚动距离
  */
-function pageOffset() {
+function pageOffset () {
     if (window.pageXOffset) {
         return {
             x: window.pageXOffset,
@@ -279,7 +278,7 @@ function pageOffset() {
  *
  *获得浏览器可视区的宽高
  */
-function windowViewOffset() {
+function windowViewOffset () {
     if (window.innerWidth) {
         return {
             width: window.innerWidth,
@@ -326,7 +325,7 @@ Element.prototype.elementPosition = function () {
  * @param {*} names 异步加载js名字
  * @param {*} path  异步加载js文件路径
  */
-function asyncScript(names, path) {
+function asyncScript (names, path) {
     for (var i = 0; i < names.length; i++) {
         var s = document.createElement('script');
         s.type = 'text/javascript';
@@ -340,7 +339,7 @@ function asyncScript(names, path) {
  *
  * @param {*} options
  */
-function ajax(options) {
+function ajax (options) {
     options = options || {};
     options.type = (options.type || "GET").toUpperCase();
     options.dataType = options.dataType || "json";
@@ -385,7 +384,7 @@ function ajax(options) {
  * @param {*} data
  * @returns
  */
-function formatParams(data) {
+function formatParams (data) {
     var arr = [];
     for (var name in data) {
         arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
@@ -400,7 +399,7 @@ function formatParams(data) {
  * @param {*} target
  * @returns
  */
-function transfer(target) {
+function transfer (target) {
     switch (target) {
         case "0":
             return '零';
@@ -431,7 +430,7 @@ function transfer(target) {
  *
  * @param {*} n
  */
-function fb(n) {
+function fb (n) {
     if (n == 1 || n == 2) {
         return 1;
     }
@@ -443,9 +442,9 @@ function fb(n) {
  *
  * @param {*} n
  */
-function fbs(n) {
-    var arr=[];
-    for(var i=1; i<=n; i++){
+function fbs (n) {
+    var arr = [];
+    for (var i = 1; i <= n; i++) {
         arr.push(fb(i));
     }
     return arr;
